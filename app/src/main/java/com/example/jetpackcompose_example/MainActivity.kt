6ca6930dfacbc35f9632bf5f1d7773f12e516902
Vlad Shesternin <valdon1202@gmail.com>
2021-02-27 08:36:21 +0200

    Start Compose Day 4

--- app/src/main/java/com/example/jetpackcompose_example/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_example/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.jetpackcompose_example
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose_example.ui.theme.JetpackComposeExampleTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
 
         }
@@ -21,17 +32,13 @@ class MainActivity : AppCompatActivity() {
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
-fun ShowPreview() {
-    Greeting(name = "Android")
+fun Show() {
+
 }
 
-@Composable
-fun Greeting(name: String) {
-    JetpackComposeExampleTheme {
-        Text(
-            color = Color.Blue,
-            text = "Hello $name!",
-            style = MaterialTheme.typography.body1,
-        )
-    }
-}
+
+
+
+
+
+
